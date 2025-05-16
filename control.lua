5e52bc78ad4ffdd9859d3d9d9e905b4392bdad5c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-05-16 23:04:48 +0100

    Add nil check

--- control.lua
+++ control.lua
@@ -3,6 +3,7 @@
 script.on_event(defines.events.on_player_changed_position,
     function(event)
         local player = game.get_player(event.player_index) -- get the player that moved
+        assert(player, "Player is nil")
         -- if they're currently controlling the character
         if player.controller_type == defines.controllers.character then
             -- and wearing our armor
